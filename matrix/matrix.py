141d441f21facdb24e862bdf5e2aeaa3410d2e5d
ruitaiS <shaoruitai@gmail.com>
2020-06-03 12:18:33 -0400

    q1 of matrix

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,20 +1,14 @@
-'''
-We have a 2^n × 2^n matrix that can only contain 0 and 1. The string S corresponding to matrix M is computed as follows:
-If all the entries of M are 0 then S = 0
-If all the entries of M are 1 then S =1
-
-Otherwise, M is divided into 4 equal matrices: M1, M2, M3, M4 with S1, S2, S3, S4 as their corresponding strings. Then S is computed as follows:
-S = 2 S1 S2 S3 S4
-
-Q1. For a 16 x 16 matrix, how many corresponding S strings can we have?
-
-Q2. Which one of the following is a valid S?
-a) 2 0 1 0 2 1 0 2 1 0 1 0 1 0
-b) 2 1 1 2 0 0 2 0 0 0 0 0 1
-c) 2 0 2 2 1 1 1 0 1 1 1 1 1
-
-Q3. Write a program that verifies if a string S is valid.
-'''
-
+#Calculates the number of possible S strings for a 2^n by 2^n matrix
+#Original question asks for n = 4
 def q1(n):
+    if n == 0: return 2
+    else: return (q1(n-1))**4
+def q1Alt(n):
+    return 2**(2**(2*n))
+
+for i in range(0, 5):
+    print(q1(i))
+    print(q1Alt(i))
+
+#def q3(s):
     
