f77354f2c7524ddb23cba5c50e448911686a3429
ruitaiS <shaoruitai@gmail.com>
2020-06-04 00:06:05 -0400

    added q3 code

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -3,12 +3,23 @@
 def q1(n):
     if n == 0: return 2
     else: return (q1(n-1))**4
-def q1Alt(n):
-    return 2**(2**(2*n))
 
-for i in range(0, 5):
-    print(q1(i))
-    print(q1Alt(i))
+print(q1(4))
 
-#def q3(s):
-    
+def q3(s):
+    s = s.replace(" ", "")
+    count = 1;
+    for c in s:
+        if c == "2":
+            count += 3
+        elif (c == "1")or(c == "0"):
+            count -= 1
+        else:
+            return False
+
+    if count != 0: return False
+    else: return True
+
+print(q3("2 0 1 0 2 1 0 2 1 0 1 0 1 0"))
+print(q3("2 1 1 2 0 0 2 0 0 0 0 0 1"))
+print(q3("2 0 2 2 1 1 1 0 1 1 1 1 1"))
