3e962f0fa833a501831af570ad1054f82280437f
ruitaiS <shaoruitai@gmail.com>
2020-06-07 21:23:36 -0400

    runs; need to validate

--- square/square.py
+++ square/square.py
@@ -9,26 +9,26 @@ def getPoints(x,y):
     return [(1,2), (x,y), p2(x,y), p3(x,y)]
 
 def checkAdj(a, b):
-    if (a[0] == a[1]) or (b[0]==b[1]): return false
+    if (a[0] == a[1]) or (b[0]==b[1]): return False
     else: return (a[0] == b[0]) ^ (a[1] == b[1]) ^ (a[0]==b[1]) ^ (a[1]==b[0])
         
 def checkOpp(a, b):
     pList = (a[0], a[1], b[0], b[1])
     for p in pList:
-        if pList.count(p) > 1: return false
-    return true
+        if pList.count(p) > 1: return False
+    return True
 
 adjPoints = [(0,1), (1,2), (2,3), (3,0)]
 oppPoints = [(0,2), (1,3)]
 
 def include(sq):
     for p in sq:
-        if (p[0]<1) or (p[1]<1) or (p[0]>10) or (p[1]>10) : return false
+        if (p[0]<1) or (p[1]<1) or (p[0]>10) or (p[1]>10) : return False
     for (a,b) in adjPoints:
-        if not checkAdj(p[a],p[b]) : return false
+        if not checkAdj(sq[a],sq[b]) : return False
     for (a,b) in oppPoints:
-        if not checkOpp(p[a],p[b]) : return false
-    return true
+        if not checkOpp(sq[a],sq[b]) : return False
+    return True
 
 def getList():
     sqList = [] 
